004ad63adc7b11cd6321146002d8e4751b5fc961
Sivanand <siva20078@gmail.com>
2012-11-28 17:42:17 +0530

    Add Mongoid+Devise+Cancan

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R3AAM::Application.config.secret_token = 'd2b8b3629eb96ba47adfb8de8b16e2bc9560f67fd41f22044b03ae058f6c0c640ad739c534da1213dbe5661aadf43b2f51b6d5827c725f4fc9fb389e5a2ae2ac'
